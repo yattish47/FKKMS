8bfa98685c4fba1caeb05fb702a2609666d677a4
yattish47 <yattish47@gmail.com>
2023-11-27 01:14:10 +0800

    Initialize Layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+
+Route::get('/test', function () {
+    return view('test');
 });
