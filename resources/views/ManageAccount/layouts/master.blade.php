ce9c8c6b39d4467f8ca41fc5f73fb8c685e6b514
yattish47 <yattish47@gmail.com>
2023-11-30 11:30:40 +0800

    register page done

--- resources/views/ManageAccount/layouts/master.blade.php
+++ resources/views/ManageAccount/layouts/master.blade.php
@@ -1,33 +1,31 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FKKMS-Login Page</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Font Awesome -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    rel="stylesheet"
-    />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
-    <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <!-- MDB -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.min.css"
-    rel="stylesheet"
-    />
-    <link rel="stylesheet" href="{{asset('css/ManageAccount/main.css')}}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.min.css" rel="stylesheet" />
+    <!-- Bootstrap Icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 
-    <link rel="icon" type="image/x-icon" href="{{asset('assets/fkkmsLogo.png')}}">
-  </head>
-  <body>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/ManageAccount/main.css') }}">
+
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/fkkmsLogo.png') }}">
+</head>
+
+<body>
     @yield('content')
 
 
@@ -36,32 +34,71 @@
     <!-- MDBootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/mdbootstrap@5.1.0/dist/js/mdb.min.js"></script>
     <!-- Bootstrap core JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
     <!-- MDB -->
-    <script
-    type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.umd.min.js"
-    ></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.umd.min.js"></script>
 
     <script>
-      // Example starter JavaScript for disabling form submissions if there are invalid fields
-      (() => {
-        'use strict';
-      
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        const forms = document.querySelectorAll('.needs-validation');
-      
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms).forEach((form) => {
-          form.addEventListener('submit', (event) => {
-            if (!form.checkValidity()) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-          }, false);
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (() => {
+            'use strict';
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            const forms = document.querySelectorAll('.needs-validation');
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms).forEach((form) => {
+                form.addEventListener('submit', (event) => {
+                    if (!form.checkValidity()) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        })();
+        document.getElementById('age').addEventListener('wheel', function(e) {
+            e.preventDefault();
         });
-      })();
-        </script>
-  </body>
+        document.getElementById('age').addEventListener('input', function(e) {
+            var input = e.target,
+                value = input.value;
+
+            if (value < 0 || Math.floor(value) != value) {
+                input.value = 0;
+            }
+        });
+        document.getElementById('age').addEventListener('keydown', function(e) {
+            if (e.key === '-' || e.key === '.') {
+                e.preventDefault();
+            }
+        });
+
+        var password = document.getElementById('password'),
+        confirmPassword = document.getElementById('confirmpassword');
+        submitbtn = document.getElementById('submitbtn');
+        errordisp = document.getElementById('errordisp');
+        confirmPassword.addEventListener('input', function(e) {
+            var errorMessage = document.getElementById('passwordError');
+            if (password.value !== confirmPassword.value) {
+                if (!errorMessage) {
+                    errorMessage = document.createElement('div');
+                    errorMessage.id = 'passwordError';
+                    errorMessage.className = 'error';
+                    errorMessage.textContent = 'Passwords do not match';
+                    errordisp.parentNode.appendChild(errorMessage);
+                    submitbtn.disabled = true;
+                }
+            } else {
+                if (errorMessage) {
+                    errorMessage.remove();
+                    submitbtn.disabled = false;
+                }
+            }
+        });
+    </script>
+</body>
+
 </html>
--- resources/views/ManageAccount/register.blade.php
+++ resources/views/ManageAccount/register.blade.php
@@ -10,59 +10,176 @@
         <div class="d-flex justify-content-center p-4">
             <div class="card text-center ">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <form class="row g-3 needs-validation" novalidate>
-                        <div class="col-md-4">
+                    <h5 class="card-title text-center signuptitle">Kiosk Participant Sign Up</h5>
+                    <form class="row g-3 needs-validation" id="signUpForm" novalidate>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
                             <div class="form-outline" data-mdb-input-init>
-                                <input type="text" class="form-control" id="validationCustom01" value="Mark"
-                                    required />
-                                <label for="validationCustom01" class="form-label">First name</label>
+                                <input type="text" class="form-control" id="validationCustom01" required />
+                                <label for="validationCustom01" class="form-label">IC Number</label>
                                 <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your IC Number</div>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
                             <div class="form-outline" data-mdb-input-init>
-                                <input type="text" class="form-control" id="validationCustom02" value="Otto"
-                                    required />
-                                <label for="validationCustom02" class="form-label">Last name</label>
+                                <input type="text" class="form-control" id="validationCustom02" required />
+                                <label for="validationCustom02" class="form-label">Name</label>
                                 <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your Name</div>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
+                            <div class="form-outline" data-mdb-input-init>
+                                <input type="text" class="form-control" id="username" required  autocomplete="username" />
+                                <label for="username" class="form-label">Username</label>
+                                <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your Username</div>
+                            </div>
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
                             <div class="input-group form-outline" data-mdb-input-init>
                                 <span class="input-group-text" id="inputGroupPrepend">@</span>
                                 <input type="text" class="form-control" id="validationCustomUsername"
-                                    aria-describedby="inputGroupPrepend" required />
-                                <label for="validationCustomUsername" class="form-label">Username</label>
-                                <div class="invalid-feedback">Please choose a username.</div>
+                                    aria-describedby="inputGroupPrepend" required placeholder="Eg: john@gmail.com" />
+                                <label for="validationCustomUsername" class="form-label">Email</label>
+                                <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your email.</div>
                             </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
+                            <select class="form-select" aria-label="Default select example"
+                                aria-describedby="inputGroupPrepend" required>
+                                <option selected value="">Select Participant Type</option>
+                                <option value="Kiosk Participant">Kiosk Participant</option>
+                                <option value="Vendor">Vendor</option>
+                            </select>
+                        </div>
+
+                        <div class="col-2">
+
+                        </div>
+
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
+                            <div class="input-group form-outline" data-mdb-input-init>
+                                <span class="input-group-text" id="inputGroupPrepend"><i class="bi bi-telephone-fill"></i></span>
+                                <input type="text" class="form-control" id="phoneNumber"
+                                    aria-describedby="inputGroupPrepend" required placeholder="Eg: 0121234566" />
+                                <label for="phoneNumber" class="form-label">Phone Number</label>
+                                <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your Phone Number.</div>
+                            </div>
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
                             <div class="form-outline" data-mdb-input-init>
-                                <input type="text" class="form-control" id="validationCustom03" required />
-                                <label for="validationCustom03" class="form-label">City</label>
-                                <div class="invalid-feedback">Please provide a valid city.</div>
+                                <input type="text" class="form-control" id="matricID" />
+                                <label for="username" class="form-label">Matric ID(FK Students)</label>
                             </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
                             <div class="form-outline" data-mdb-input-init>
-                                <input type="text" class="form-control" id="validationCustom05" required />
-                                <label for="validationCustom05" class="form-label">Zip</label>
-                                <div class="invalid-feedback">Please provide a valid zip.</div>
+                                <input type="text" class="form-control" id="nationality" required />
+                                <label for="nationality" class="form-label">Nationality</label>
+                                <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your Nationality.</div>
                             </div>
                         </div>
-                        <div class="col-12">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-                                <label class="form-check-label" for="invalidCheck">Agree to terms and conditions</label>
-                                <div class="invalid-feedback">You must agree before submitting.</div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
+                            <div class="form-outline" data-mdb-input-init>
+                                <input type="number" class="form-control" id="age" min="0" step="1" required style="appearance: none; -moz-appearance: textfield;"/>
+                                <label for="age" class="form-label">Age</label>
+                                <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your age.</div>
                             </div>
                         </div>
-                        <div class="col-12">
-                            <button class="btn btn-primary" type="submit" data-mdb-ripple-init>Submit form</button>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
+                            <div class="form-outline" data-mdb-input-init>
+                                <input type="password" class="form-control" id="password"  autocomplete="new-password" required/>
+                                <label for="password" class="form-label">Password</label>
+                                <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your password.</div>
+                            </div>
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-2">
+
+                        </div>
+                        <div class="col-8 mt-4">
+                           
+                            <div class="form-outline" data-mdb-input-init>
+                                <input type="password" class="form-control" id="confirmpassword" required/>
+                                <label for="confirmpassword" class="form-label">Confirm Password</label>
+                                <div class="valid-feedback">Looks good!</div>
+                                <div class="invalid-feedback">Please enter your confirm password.</div>
+                               
+                            </div>
+                            <div id="errordisp"></div>
+                        </div>
+                        <div class="col-2">
+                           
+                        </div>
+                        <div class="col-12 mt-4">
+                            <button class="btn signupbtn btn-primary" id="submitbtn" type="submit" data-mdb-ripple-init>Submit
+                                form</button>
                         </div>
                     </form>
-                    <button type="button" class="btn btn-primary">Button</button>
+                    <p class="card-text mt-3">Already have an account? You can <a href="{{ route('login') }}"><u>login
+                                here</u></a></p>
                 </div>
             </div>
         </div>
